3f124c1eb22df1423556365a8204ac658e558ead
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-06-06 16:58:32 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //imports
 import adminprofile from "./components/admin_profile/adminprofile";
 import home from "./components/home/home"
-<<<<<<< HEAD
-import login from "./components/login/login"
-import Nav from "./components/navbar/navbar"
 
-function App() {
-    return (
-        <BrowserRouter key={App.BrowserRouter}>
-            <Nav/>
-            <Route path="/" exact component={home} key={App.home}></Route>
-            <Route path="/admin" exact component={adminprofile} key={App.adminprofile}></Route>
-            <Route path="/login" exact component={login} key={App.login}></Route>
-        </BrowserRouter>
-=======
 import Navbar from "./components/navbar/navbar"
 
 function App() {
@@ -33,7 +21,7 @@ function App() {
             </main>
 
         </Router>
->>>>>>> c438167a53fd20ade61964d883c5fad937bc8351
+
 
     )
 }
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -5,15 +5,9 @@ import Button from '@mui/material/Button';
 function navbar() {
   return (
     <div>
-<<<<<<< HEAD
-     
-     <h1>Navbar</h1>
-     
-=======
       
     <Button variant="contained">Hello World</Button>;
       
->>>>>>> c438167a53fd20ade61964d883c5fad937bc8351
     </div>
   )
 }
